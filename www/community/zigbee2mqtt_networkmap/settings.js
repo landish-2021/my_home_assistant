80f701aa826c440e44b6d25fc4d89fc14a133a9f
Andrey <landish.osnk@mail.ru>
2021-10-03 18:28:47 +0700

    my home
    config

---
+++ www/community/zigbee2mqtt_networkmap/settings.js
@@ -0,0 +1,2 @@
+var webhook_trigger_update_id = 'c9ce25ede2f4f75b62e43dd54681a9bc14a1fe27b47e7f3dba1173d9bc0ce63c';
+var webhook_check_update_id = '48c93ae08528a9d4c09b592e4cd489659d1ccb77cbde7354c715b10a1caf6dc2';
